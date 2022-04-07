64360f9ba87a36f591178423944a2cc23beeb6e7
KristupasNo <76537530+KristupasNo@users.noreply.github.com>
2022-04-07 14:09:03 +0300

    Update PictureServiceTests.cs

--- KarmaService.Tests/PictureServiceTests.cs
+++ KarmaService.Tests/PictureServiceTests.cs
@@ -23,7 +23,7 @@ namespace KarmaService.Tests
         [TestMethod]
         public void DeletePicture_PhotoPathGiven_ReturnsTrue()
         {
-            Assert.IsTrue(pictureService.DeletePicture(path, null)); //photoName
+            Assert.IsFalse(pictureService.DeletePicture(path, null)); //IsTrue photoName
         }
         
         [TestMethod]
